a3bf28a661fec1685f6dc7c56ce5acfb04686303
Sean Kramer <seandominickramer@gmail.com>
2020-04-24 12:07:06 -0400

    Addd Form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import Form from "./Form"
+
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
+<Form />
     </>
   );
 };
---
+++ src/Form.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <label>
+        Special Instructions:
+      <input type='text'/>
+      </label>
+      
+    </form>
+  )
+}
+
+export default Form;

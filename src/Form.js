176a7b9a5cc31d8b3eae7a9452e2e4450a644737
Sean Kramer <seandominickramer@gmail.com>
2020-04-24 15:35:32 -0400

    Add checkboxes

--- src/Form.js
+++ src/Form.js
@@ -1,19 +1,44 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
+import Pizza from "./Pizza.jpg";
 
 const Form = () => {
   return (
-    <div className='formDiv'>
-    <form className=''>
-      <label>
-        Special Instructions:
-      <input type='text'/>
-      </label>
-      
-    </form>
-    </div>
-
-  )
-}
+    <div className="formDiv">
+      <header>Build Your Pizza</header>
+      {/* <img src={Pizza}/> */}
+        <form className="options-form">
+          <label>
+            Size:
+            <select className="size-options">
+              <option value="large">Large</option>
+              <option value="medium">Medium</option>
+              <option value="small">Small</option>
+            </select>
+          </label>
+        <div classname='checkboxes'>
+          <h3>Select Your Toppings:</h3>
+              <input type='checkbox' name='pepperoni' value='Pepperoni'/><label>Pepperoni</label>
+              <br></br>
+              <input type='checkbox' name='sausage' value='
+              Sausage'/><label>Sausage</label>
+             <br></br>
+              <input type='checkbox' name='mushroom' value='Mushroom'/><label>Mushroom</label>
+              <br></br>
+              <input type='checkbox' name='pineapple' value='Pineapple'/><label>Pineapple</label>
+              <br></br>
+          </div>
+          <label>
+            Special Instructions:
+            <input type="text" />
+          </label>
+          <label>
+            Your Name:
+            <input type="text" />
+          </label>
+        </form>
+      </div>
+  );
+};
 
 export default Form;

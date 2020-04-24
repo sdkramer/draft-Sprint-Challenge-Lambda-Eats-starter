40510be686f868abbde79048135371e938e4d0cb
Sean Kramer <seandominickramer@gmail.com>
2020-04-24 16:40:47 -0400

    Correct bugs on changehandlers

--- src/Form.js
+++ src/Form.js
@@ -7,7 +7,7 @@ const Form = () => {
   const [inputValue, setInputValue] = useState('')
 
   const changeHandler = event => {
-    console.log(event)
+    console.log(event.target.value)
   }
   return (
     <div className="formDiv">
@@ -17,30 +17,30 @@ const Form = () => {
           <label>
             Size:
             <select className="size-options">
-              <option value="large">Large</option>
-              <option value="medium">Medium</option>
-              <option value="small">Small</option>
+              <option value="large" onChange={changeHandler}>Large</option>
+              <option value="medium" onChange={changeHandler}>Medium</option>
+              <option value="small" onChange={changeHandler}>Small</option>
             </select>
           </label>
-        <div classname='checkboxes'>
+        <div classname='checkboxes' onChange={changeHandler}>
           <h3>Select Your Toppings:</h3>
-              <input type='checkbox' name='pepperoni' value='Pepperoni'/><label>Pepperoni</label>
+              <input type='checkbox' name='pepperoni' value='Pepperoni' onChange={changeHandler}/><label>Pepperoni</label>
               <br></br>
               <input type='checkbox' name='sausage' value='
-              Sausage'/><label>Sausage</label>
+              Sausage' onChange={changeHandler}/><label>Sausage</label>
              <br></br>
-              <input type='checkbox' name='mushroom' value='Mushroom'/><label>Mushroom</label>
+              <input type='checkbox' name='mushroom' value='Mushroom' onChange={changeHandler}/><label>Mushroom</label>
               <br></br>
-              <input type='checkbox' name='pineapple' value='Pineapple'/><label>Pineapple</label>
+              <input type='checkbox' name='pineapple' value='Pineapple' onChange={changeHandler}/><label>Pineapple</label>
               <br></br>
           </div>
           <label>
             Special Instructions:
-            <input type="text" />
+            <input type="text" onChange={changeHandler}/>
           </label>
           <label>
             Your Name:
-            <input type="text" />
+            <input type="text" onChange={changeHandler}/>
           </label>
         </form>
       </div>

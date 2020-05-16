9272e61cdf817ef83586bd84a8bb4616ce83b5b1
Sean Kramer <seandominickramer@gmail.com>
2020-05-15 21:58:16 -0400

    Add test for trying to submit form without name input

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -5,31 +5,29 @@ describe("First test", () => {
 });
 
 describe("Tests for form inputs and submit", () => {
-before(function () {
-  cy.visit('http://localhost:3000/pizza') 
+  before(function () {
+    cy.visit("http://localhost:3000/pizza");
+  });
+
+  it("inputs name into name input", () => {
+    cy.get(":nth-child(4) > input")
+      .type("Amanda")
+      .should("have.value", "Amanda")
+      .clear()
+      .type("a");
+    cy.contains("Name must include at least 2 characters");
+    cy.get(":nth-child(4) > input")
+    .clear();
+  });
+
+  it("checks to see if user can select toppings", () => {
+    cy.get('[name="pepperoni"]').check().should("be.checked");
+
+    cy.get('[name="sausage"]').check().should("be.checked");
+  });
+
+  it("checks to see if a user can submit the form", () => {
+    cy.get("form").submit();
+    cy.contains("Please enter your name")
+  });
 });
-
-it('inputs name into name input', () => {
-  cy.get(':nth-child(4) > input')
-  .type('Amanda')
-  .should('have.value', 'Amanda')
-  .clear()
-  .type('a')
-      cy.contains("Name must include at least 2 characters")
-})
-
-it('checks to see if user can select toppings', () => {
-  cy.get('[name="pepperoni"]')
-  .check()
-  .should('be.checked')
-
-  cy.get('[name="sausage"]')
-  .check()
-  .should('be.checked')
-})
-
-it("checks to see if a user can submit the form", () => {
-  cy.get('form').submit();
-});
-
-})
